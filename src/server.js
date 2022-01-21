e2e07e7a888d12fdb906cc6e6eb472f7a3c38cdb
prinako <nyankonton122@gmail.com>
2022-01-21 17:02:12 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+app.use(cors())
+
 const mongoose = require("mongoose");
 const MongoStore = require("connect-mongo");
 const bodyparser = require("body-parser");
